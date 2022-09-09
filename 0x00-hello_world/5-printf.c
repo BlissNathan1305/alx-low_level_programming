a1bebd69bf8d9bcf30dff61dc2e8d707b56d9c4d
BlissNathan1305 <sambless44@gmail.com>
2022-09-08 19:36:42 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/**
- * main - prints the phrase with proper grammar,
+/**                                                                    * main - prints the phrase with proper grammar,
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
-{
-	printf("with proper grammar, but the  outcome is a piece of art,\n");
-	return (0);
+{                                                                             printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
