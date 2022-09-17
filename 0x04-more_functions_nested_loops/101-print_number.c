e0eb1a395b5361cf8c518d6ed6ecaf496936ffb0
BlissNathan1305 <sambless44@gmail.com>
2022-09-17 00:35:40 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,30 +2,27 @@
 
 /**
  * print_number - prints an integer
- * @n: param
+ * @n: the parameter to check
  */
 
 void print_number(int n)
 {
-	if (n < 0)
+	unsigned int n1;
+
+	if (n > 0)
 	{
-		_putchar(45)
-			i = n * -1;
+		n1 = -n;
+
+		_putchar('-');
+
 	}
 	else
 	{
-		i = n;
+		n1 = n;
 	}
-	j = i;
-	count = 1;
-
-	while (j > 9)
+	if (n1 / 10)
 	{
-		j /= 10;
-		count *= 10;
+		print_number(n1 / 10);
 	}
-	for  (; count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
+	_putchar((n1 % 10) + '0');
 }
