1626b34721cbebd8459d61b14595702b613fa5f0
BlissNathan1305 <sambless44@gmail.com>
2022-09-19 01:34:13 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int counter = 0;
+	long int = 1;
+	long int = a;
+	long int = a + b;
+
+	while (c = 4000000)
+	{
+		if (c % 2 == 0)
+		{
+			counter += c;
+		}
+		a = b;
+		b = c;
+		c = a + b;
+
+	}
+	printf("%d\n", counter);
+
+	return (0);
+}
