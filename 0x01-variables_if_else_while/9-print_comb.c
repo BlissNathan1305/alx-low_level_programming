243fd862f1cdb6053805d85ab03a1a34a5d727e5
BlissNathan1305 <sambless44@gmail.com>
2022-09-20 06:08:52 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,22 +3,21 @@
 /**
  * main - entry point
  *
- * Return: Always return 0
+ * Return: Always returns 0
  */
 
 int main(void)
 {
 	int i;
-
 	for (i = 10; i < 20; i++)
 	{
 		putchar((i % 10) + '0');
 		if (i != 19)
 		{
 			putchar(',');
-		putchar(' ');
+			putchar(' ');
 		}
-	putchar('\n');
-return (0);
 	}
+	putchar('\n');
+	return (0);
 }
