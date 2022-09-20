674241b9e3aad83008e398268c27d34805069207
BlissNathan1305 <sambless44@gmail.com>
2022-09-20 03:03:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:Always returns 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
+
