4d4fa4d0c4c3d0a7b47c9717e41f6bcd46e5d05c
BlissNathan1305 <sambless44@gmail.com>
2022-09-28 06:14:18 -0700

    main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -15,7 +15,9 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
-
+int is_sqrt(int n, int square);
+int is_prime(int n, int i);
+int palindrome_check(char *s, int len, int i);
 
 #endif /* MAIN_H */
 
