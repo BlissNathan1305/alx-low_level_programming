1f5b12bd7a9c6bd4f5fb1cbcad6bee5a138dc614
BlissNathan1305 <sambless44@gmail.com>
2022-09-28 01:19:58 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_recursion(int n)
 		return (-1);
 	else if (n == 0 || n == 1)
 		return (n);
-	return (_is_sqrt(n, square));
+	return (is_sqrt(n, square));
 }
 /**
  * is_sqrt - function to check whether it is a natural square root or not
