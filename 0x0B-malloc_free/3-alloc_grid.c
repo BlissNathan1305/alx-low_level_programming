373d05ef5bc338a7161721d08b5e0d678e0deaa6
BlissNathan1305 <sambless44@gmail.com>
2022-10-05 05:09:50 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 	grid = malloc(height * sizeof(int *));
-	if (grip == NULL)
+	if (grid == NULL)
 	{
 		free(grid);
 		return (NULL);
