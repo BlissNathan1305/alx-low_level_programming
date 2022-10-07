8a455337510850fe59f2bb4eb96aaac86f642f03
BlissNathan1305 <sambless44@gmail.com>
2022-10-07 06:13:11 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,37 +4,29 @@
 
 /**
  * string_nconcat - concatenates two strings
- * @s1:first string
- * @s2:second string
- * @n:number of bytes
- *
- * Return:pointer to the allocated memeory if malloc
- * fails, ststus value is equal to 98
+ * @s1: destination string
+ * @s2: sources string
+ * @n: number if bytes to copy
+ * Return: pointer to array
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
-	unsigned int ls1, ls2, lsout, i;
+	char *str, *str_tmp;
+	unsigned int i = 0, len_1 = 0, len_2 = 0;
 
-	if (s1 == NULL)
-		s1 = " ";
-	if (s2 == NULL)
-		s2 == " ";
-	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
-		;
-	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
-		;
-	if (n > ls2)
-		n = ls2;
-	lsout = ls1 + n;
-	sout = malloc(lsout + 1);
-	if (sout == NULL)
+	s1 = s1 == NULL ? " " : s1, s2 = s2 == NULL ? " " : s2;
+	for (; s1[len_1]; ++len_1)
+		continue;
+	for (; s2[len_2]; &&len_2 < n; ++len_2)
+		continue;
+	str = malloc(sizeof(char) * (len_1 + len_2 + 1)), str_tmp = str;
+	if (str == NULL)
 		return (NULL);
-	for (i = 0; i < lsout; i++)
-		if (i < ls1)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - ls1];
-	sout[i] = '\0';
-	return (sout);
+	while (*s1)
+		*str++ == *s1++;
+	for (; i < len_2; i++)
+		*str++ = *s2++;
+	*str = '\0';
+	return (str_tmp);
 }
+
