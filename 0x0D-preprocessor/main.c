c672533013097e97077404aad5ba40d454094b9a
BlissNathan1305 <sambless44@gmail.com>
2022-10-10 01:56:48 -0700

    main.c

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,17 @@
+#include "0-object_line_macro.h"
+#include "0-object_line_macro.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int s;
+
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
+}
